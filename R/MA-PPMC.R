59d2b0dc02909c3eb83a5de67ae31c5e54a74247
JeffreyCHoover <43454055+JeffreyCHoover@users.noreply.github.com>
2019-09-07 13:11:33 -0500

    alter tau_d code

--- R/MA-PPMC.R
+++ R/MA-PPMC.R
@@ -47,7 +47,7 @@ ppmc_ma <- function(fileName, meta, fixed = FALSE)
   } else {
     dataList = list(
       n = length(meta$yi),
-      tau = meta$tau2,
+      tau = 1,
       es = meta$b,
       es_se = meta$se,
       w = meta$vi,
@@ -62,11 +62,7 @@ ppmc_ma <- function(fileName, meta, fixed = FALSE)
     w_mean <- mean(w)
 
     # priors
-    if(tau == 0.1){
-      tau_d = 0.1
-    } else {
-      tau_d ~ dunif(0.1, 1)
-    }
+    tau_d ~ dunif(0.1, tau)
     for(i in 1:n) {
       weights[i] ~ dunif(min(w), max(w))#dnorm(w_mean, w_sd)
       effects[i] ~ dnorm(es, es_sd)

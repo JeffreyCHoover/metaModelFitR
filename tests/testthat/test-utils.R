2b687093647e36267fae0882b17801a040bc03b9
JeffreyCHoover <43454055+JeffreyCHoover@users.noreply.github.com>
2020-01-12 14:13:21 -0600

    Update test-utils.R

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,24 +1,23 @@
-context("test-utility_functions")
+context ("test-utility_functions")
 
-test_that("meta_retrieve works", {
-  es <- c(0.5, 0.6, 0.7, 0.8, 0.9)
-  v <- c(.05, .1, .08, .1, .03)
+test_that ("meta_retrieve works", {
+  es <- c (0.5, 0.6, 0.7, 0.8, 0.9)
+  v <- c (.05, .1, .08, .1, .03)
   w <- 1 / v
-  d <- tibble::tibble(es, v)
-  re_meta <- metafor::rma(data = d, yi = es, vi = v, method = "FE")
+  d <- tibble::tibble (es, v)
+  re_meta <- metafor::rma (data = d, yi = es, vi = v, method = "FE")
 
-  test <- meta_retrieve(re_meta, fixed = TRUE)
+  test <- meta_retrieve (re_meta, fixed = TRUE)
 
   testthat::expect_equal(test$weights, w)
   testthat::expect_equal(as.vector(test$effect), es)
   testthat::expect_equal(test$k, rep(length(es), length(es)))
 })
 
-test_that("calc_ppp works", {
-  discrepancy <- matrix(data = c(4), nrow = 1, ncol = 1)
-  simulated_discrepancy <- data.frame(c(2, 3, 5, 6, 7)) %>%
-    dplyr::rename(sim_disc = 1)
+test_that ("calc_ppp works", {
+  discrepancy <- 4
+  simulated_discrepancy <- c(2, 3, 5, 6, 7)
 
-  testthat::expect_equal(.4, calc_ppp(discrepancy = discrepancy,
+  testthat::expect_equal (.4, calc_ppp(c(4),
                             simulated_discrepancy = simulated_discrepancy))
 })
